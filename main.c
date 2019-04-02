ade81a078d7dbacfceeaff06cc5df72f0add5065
Peter Thring <p3thring@gmail.com>
2019-04-02 22:10:24 +0000

    Create main.c

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/*/maybe
+//function to input string and convert to all caps
+char input_string(char *string)
+/*/
+
+//function to convert string to all caps
+char check_all_caps(char *string, unsigned int n);
+
+
+
+//FUNCTIONS FOR ROTATION CYPHER//
+
+//functon to encode string with rotation cypher
+char rotation_encode(char *string, unsigned int n, int cypher);
+
+//function to decode string with rotation cypher
+char rotation_decode(char *string, unsigned int n);
+
+//function to decode string without rotation cypher
+char rotation_decrypt(char *string, )
+
+
+
+//FUNCTIONS FOR SUBSTITUTION CYPHER
+
+//function to encode string with substitution cypher
+char substitution_cypher(char *string, unsigned int)
+
+
+//MAIN CODE
+int main() {
+  //string in
+  //char string[];
+  //int cypher; 
+  //convert to all caps (check_all_caps(string, length))
+  
+  
+  return 0;   //end of main code
+}
