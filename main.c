6b2370f22389c64a18255c5074e8da82cd20f55c
Peter Thring <p3thring@gmail.com>
2019-04-03 00:54:45 +0000

    commit from first lab labWk6

--- main.c
+++ main.c
@@ -9,10 +9,10 @@ char check_all_caps(char *string, unsigned int string_length);
 
 
 //function to convert each character in string from ASCII to a number from 0 to 25
-char convert_from_ASCII(char *string, unsigned int string_length, unsigned int *stringN);
+char convert_from_ASCII(char *string, unsigned int string_length, int *stringN);
 
 //function to convert each character in string from a number from 0 to 25 to ASCII
-char convert_to_ASCII(char *string, unsigned int string_length, unsigned int *stringN);
+char convert_to_ASCII(char *string, unsigned int string_length, int *stringN);
 
 
 //FUNCTIONS FOR ROTATION CYPHER//
@@ -56,6 +56,8 @@ int main() {
     char string[1024];  //unused = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'};
     scanf("%s", string);  //string in
     
+    int stringN[1024];
+    
     //get number of characters in string
     unsigned int string_length = strlen(string);
           
@@ -65,16 +67,16 @@ int main() {
     
     
     //convert each character in string from ASCII to a number from 1 to 25
-    convert_from_ASCII(string, string_length, unsigned int *stringN);
+    convert_from_ASCII(string, string_length, stringN);
     
     
     
     //encrypt with rotation cypher
-    printf("%s\n", string);
+    rotation_encode(string, string_length, rotation_cypher);
     
     
     //convert each character in string from a number from 0 to 25 to ASCII
-    convert_to_ASCII(string, string_length, unsigned int *stringN);
+    convert_to_ASCII(string, string_length, stringN);
     
     
     //print cypher key and encrypted string
@@ -128,40 +130,107 @@ char check_all_caps(char *string, unsigned int string_length)
         }      
         
     }
+    for ( int n = 0 ; n < string_length ; n++ ) {
+        printf("%d", (int)string[n]);
+    }
+    printf("\n");
     
     return 1;
 }
 
 
+
 //function to convert each character in string from ASCII to a number from 1 to 25
-char convert_from_ASCII(char *string, unsigned int string_length, unsigned int *stringN)
+char convert_from_ASCII(char *string, unsigned int string_length, int *stringN)
 {
     //convert each character in string from ASCII value to number from 0 to 25
     for ( int n ; n < string_length ; n++ ) {
         
         //if the character at string[n] is an uppercase letter convert
         if ( string[n] >= 65 && string[n] <= 90 ) {
-            string[n] -= 65;
+            stringN[n] = string[n] - 65;
         }
         
     }
+    
+    for ( int n = 0 ; n < string_length ; n++ ) {
+        printf("%d", stringN[n]);
+    }
+    printf("\n");
+    
     return 1;
 }
 
 //function to convert each character in string from a number from 0 to 25 to ASCII
-char convert_to_ASCII(char *string, unsigned int string_length, unsigned int *stringN)
+char convert_to_ASCII(char *string, unsigned int string_length, int *stringN)
 {
     //convert each character in string from number from 0 to 25 to ASCII value
     for ( int n ; n < string_length ; n++ ) {
         
         //if the character at string[n] is an uppercase letter convert
        if ( string[n] >= 65 && string[n] <= 90 ) {
-            string[n] += 65;
+            stringN[n] = string[n] + 65;
         }
         
     }   
+    
+    for ( int n = 0 ; n < string_length ; n++ ) {
+        printf("%d", stringN[n]);
+    }
+    printf("\n");
+    
     return 1;
 }
+
+
+
+//FUNCTIONS FOR ROTATION CYPHER//
+
+//functon to encode string with rotation cypher
+char rotation_encode(char *string, unsigned int string_length, int rotation_cypher)
+{
+    for ( int n = 0 ; n < string_length ; n++ ) {
+        if ( string[n] >= 0 && string[n] <= 25 ) {
+            string[n] = ( string[n] + rotation_cypher + 26 ) % 26;
+        }
+    }
+    printf("%s\n", string);
+    return 1;
+}
+
+
+//function to decode string with rotation cypher
+char rotation_decode(char *string, unsigned int string_length, int rotation_cypher)
+{
+    for ( int n = 0 ; n < string_length ; n++ ) {
+        if ( string[n] >= 0 && string[n] <= 25 ) {
+            string[n] = ( string[n] - rotation_cypher + 26 ) % 26;
+        }
+    }    
+    printf("%s\n", string);
+    return 1;
+}
+
+
+//function to decode string without rotation cypher
+char rotation_decrypt(char *string, unsigned int string_length)
+{
+    
+    return 1;
+}
+
+
+
+//FUNCTIONS FOR SUBSTITUTION CYPHER
+
+//function to encode string with substitution cypher
+char substitution_encode(char *string, unsigned int string_length, char *substitution_cypher, unsigned int cypher_n);
+
+//function to decode string with substitution cypher
+char substitution_decode(char *string, unsigned int string_length, char *substitution_cypher, unsigned int cypher_n);
+
+//function to decode string without substitution cypher
+char substitution_decrypt(char *string, unsigned int string_length);
 
 
 
