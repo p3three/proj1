cae72bd7319d4638618bc63e089d95364e465f47
Peter Thring <p3thring@gmail.com>
2019-04-12 04:57:46 +0000

    non-functional commit

--- main.c
+++ main.c
@@ -100,6 +100,7 @@ int main() {
     
     //declare rotation_cypher as an integer
     int rotation_cypher = 0;
+    int *pointer_rotation_cypher = &rotation_cypher;
     
     //declare array to hold substitution cypher
     char substitution_cypher[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'};
@@ -118,7 +119,7 @@ int main() {
             cypher_type = 2;
             break;
         case 0:
-            printf("CASE: ERROR\n");
+            printf("CASE: DECRYPT\n");
             break;
     }
     if ( cypher_type != 0 ) {
@@ -127,18 +128,23 @@ int main() {
 
     
     //declare string //unused// as an array of characters 
-    char string[1024];  //unused// = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'};
-    string[1024] = 0;
+    char string[] = "TVU TVAOTH: AOL KHAH IYVBNOA AV BZ IF AOL IVAOHU ZWPLZ WPUWVPUAZ AOL LEHJA SVJHAPVU VM AOL LTWLYVY'Z ULD IHAASL ZAHAPVU. DL HSZV RUVD AOHA AOL DLHWVU ZFZALTZ VM AOPZ KLHAO ZAHY HYL UVA FLA VWLYHAPVUHS. DPAO AOL PTWLYPHS MSLLA ZWYLHK AOYVBNOVBA AOL NHSHEF PU H CHPU LMMVYA AV LUNHNL BZ, PA PZ YLSHAPCLSF BUWYVALJALK. IBA TVZA PTWVYAHUA VM HSS, DL'CL SLHYULK AOHA AOL LTWLYVY OPTZLSM PZ WLYZVUHSSF VCLYZLLPUN AOL MPUHS ZAHNLZ VM AOL JVUZAYBJAPVU VM AOPZ KLHAO ZAHY. THUF IVAOHUZ KPLK AV IYPUN BZ AOPZ PUMVYTHAPVU.";
+//    char string[1024];  //unused// = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'};
+
     
     //get number of characters in string 
     unsigned int string_length = strlen(string);
 
     //read string from INPUT
-    read_string(string, string_length);
+//    read_string(string, string_length);
     
+    printf("STRING_LENGTH: %d\n", string_length);
+    
+    printf("#STRING: \n");
     for ( int i = 0 ; i < string_length ; i++ ) {
-        printf("STRING: %c\n", string[i]);
+        printf("%c", string[i]);
     }
+    printf("\n#END\n");
 
     
 /*
@@ -147,14 +153,27 @@ int main() {
     
     
     //verify string is valid (A,B,C,... )
+*/
     
-    //? int stringN[1024];
+    int stringN[1024];
     
     //convert each character in string from ASCII to a number from 1 to 25
     convert_from_ASCII(string, string_length, stringN);
     
-    //encrypt with rotation cypher 
-    rotation_encode(string, string_length, rotation_cypher);
+    switch (cypher_type) {
+        case 1:
+            //encrypt with rotation cypher
+            rotation_cypher(string, string_length, rotation_cypher);
+            break;
+        case 2:
+            //encrypt with substitution cypher
+            substitution_cypher(string, string_lenght, substitution_cypher);
+            break;
+        case 0:
+            //decript without cypher
+            decrypt(string, string_length);
+            break;
+    }
     
     
     //convert each character in string from a number from 0 to 25 to ASCII
@@ -162,13 +181,13 @@ int main() {
     
     
     //print cypher key and encrypted string to OUTPUT
-    printf("Key is: %d\n", rotation_cypher);
+//    printf("Key is: %d\n", rotation_cypher);
      
-    printf("The length of the string is: %d\n", string_length);
+//    printf("The length of the string is: %d\n", string_length);
      
-    printf("The encrypted string is: %s\n", string);
+    printf("THE_STRING_IS:\n%s\n", string);
      
-*/  return 0; 
+    return 0; 
 }
 //************************************************//
 
@@ -241,33 +260,35 @@ char read_encryption_key(int *rotation_cypher, char *substitution_cypher) {
 
 //function to read string from INPUT 
 unsigned int read_string(char *string, unsigned int string_length) 
-{ 
+{
     FILE *INPUT;
     
     INPUT = fopen("INPUT","r");
-
+    
     if ( INPUT == NULL ) {
         perror("fopen()");
     }
     
-    int count = -1;  //initiate integer for counting loops
+    int count = 0;  //initiate integer for counting loops
     unsigned int rtn = 0; //initiate variable to hold value for return
+    int scan = 0;
     
-    char str[1024];
+//    char str[1024];
     
     rewind(INPUT);
     
-    if ( ( fscanf(INPUT,"#INPUT:\n%s[\n]", str) ) == 0 ) {
-        for ( count = 0 ; count < 100 ; count++ ) {
-            fscanf(INPUT,"#KEY: %s[\n]",str);
-            if ( str[count] != 0) {
-                break;
-            }
-        }
+    //for ( count = 0 ; count < 100 ; count++ ) {
+    scan = fscanf(INPUT,"#INPUT: %s", string);
+    while ( ( scan == 0 ) && count < 100 ) {
+        scan = fscanf(INPUT,"#INPUT: %s", string);
+        count++;
     }
+
+  //  }
     
     printf("TEST_COUNT: %d\n",count);
-        
+    
+    /*
     for ( count = 0 ; string[count] != '#' ; count++) {  //until end of file reached 
                
         fscanf(INPUT,"%c", &string[count]);
@@ -276,22 +297,15 @@ unsigned int read_string(char *string, unsigned int string_length)
         
         count++;
     }
+    */
     clearerr(INPUT);
     
     printf("STRING_IS: %s\n", string);
     
-     
-    if ( count < string_length ) {
-        rtn = count;
-    } 
-     else {
-        string[string_length] = 0;
-        rtn = 0;
-    }
     return rtn;
 }
 
-/*
+
 //function to convert string to all caps
 char check_all_caps(char *string, unsigned int string_length)
 {
@@ -320,7 +334,7 @@ char convert_from_ASCII(char *string, unsigned int string_length, int *stringN)
 
         //if the character at string[n] is an uppercase letter convert
         if ( string[n] >= 65 && string[n] <= 90 ) {
-            stringN[n] = (int)string[n] - 65;
+            stringN[n] -= 65;
         }
     }
     for ( int n = 0 ; n < string_length ; n++ ) {
@@ -351,6 +365,15 @@ char convert_to_ASCII(char *string, unsigned int string_length, int *stringN)
 
 //FUNCTIONS FOR ROTATION CYPHER//
 
+//function to determine if encode or decode
+char rotation_cypher(char *string, unsigned int string_length, int rotation_cypher)
+{
+    //if encode return case 1;
+    //if decode return case 2;
+    //if ERROR return case 0;
+    return 0;
+}
+
 //functon to encode string with rotation cypher//
 char rotation_encode(char *string, unsigned int string_length, int rotation_cypher)
 {
@@ -377,7 +400,7 @@ char rotation_decode(char *string, unsigned int string_length, int rotation_cyph
 }
 
 
-
+/*
 //function to decode string without rotation cypher//
 char rotation_decrypt(char *string, unsigned int string_length)
 {
